227e33c3756682ccbc43a715c964a1a8ed63edd0
mtomcal <mandyt2008@gmail.com>
2015-09-14 21:55:53 +0000

    Initial rails app

---
+++ api/spec/models/user_spec.rb
@@ -0,0 +1,12 @@
+require 'spec_helper'
+
+describe User do
+    before { @user = FactoryGirl.build(:user) }
+    subject { @user }
+    it { should validate_presence_of(:email) }
+    it { should validate_uniqueness_of(:email) }
+     it { should validate_presence_of(:password) }
+    it { should validate_confirmation_of(:password) }
+    it { should allow_value('example@domain.com').for(:email) }
+
+end
